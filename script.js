5277640bfcf2a2a81a3fe1f9787aecd3fe9b1feb
Constantinos Yenis <cyenis@outlook.com>
2023-03-21 14:48:27 +0100

    History table

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Get the table body where results will be displayed
+const resultsTable = document.getElementById("results-table");
+
+// Load results from local storage
+const results = JSON.parse(localStorage.getItem("results")) || [];
+
+// Render existing results in table
+results.forEach((result) => {
+    const row = resultsTable.insertRow();
+    row.innerHTML = `<td>${result.date}</td><td>${result.closedChats}</td><td>${result.csat}</td><td>${result.position}</td><td>${result.bonus.toFixed(
+        2
+    )} \u20AC</td>`;
+});
+
+// Clear history button
+const clearHistoryBtn = document.getElementById("clear-history-btn");
+clearHistoryBtn.addEventListener("click", () => {
+    localStorage.removeItem("results");
+    resultsTable.innerHTML = "";
+});
+
 function calculateBonus() {
     const closedChats = document.getElementById("closedChats").value;
     const csat = document.getElementById("csat").value;
@@ -5,38 +26,41 @@ function calculateBonus() {
 
     let bonus = 0;
     let gif = "";
-    let thresholdChats = 700;
-    let thresholdCsat = 90;
 
+    // Calculate bonus based on position and performance
     if (position === "teamLead") {
-        // Team leads have to dedicate only 70% of their time to chats
-        thresholdChats = 700 * 0.8;
-        thresholdCsat = 90;
-    }
-
-    if (closedChats >= thresholdChats && csat >= thresholdCsat) {
-        bonus =
-            50 +
-            ((closedChats - thresholdChats) / 10) * 2 +
-            (csat - thresholdCsat) * 10 * 3;
-        document.getElementById(
-            "bonus"
-        ).textContent = `Your bonus is \u20AC${bonus.toFixed(2)}.`;
-
-        if (bonus >= 300) {
-            gif = "https://media.giphy.com/media/i3D35lrOfyzTkg5a9o/giphy.gif";
-        } else if (bonus >= 200) {
-            gif = "https://media.giphy.com/media/i3D35lrOfyzTkg5a9o/giphy.gif";
-        } else if (bonus >= 100) {
-            gif = "https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif";
-        } else {
-            gif = "https://media.giphy.com/media/3oz8xAFtqoOUUrsh7W/giphy.gif";
+        if (closedChats >= 490 && csat >= 90) {
+            bonus =
+                50 + ((closedChats - 490) / 10) * 2 + (csat - 90) * 10 * 3 * 0.7;
         }
     } else {
-        document.getElementById(
-            "bonus"
-        ).textContent = "Sorry, you don't qualify for a bonus this month.";
+        if (closedChats >= 700 && csat >= 90) {
+            bonus = 50 + ((closedChats - 700) / 10) * 2 + (csat - 90) * 10 * 3;
+        }
+    }
+
+    // Determine GIF based on bonus amount
+    if (bonus >= 300) {
+        gif = "https://media.giphy.com/media/i3D35lrOfyzTkg5a9o/giphy.gif";
+    } else if (bonus >= 200) {
+        gif = "https://media.giphy.com/media/JpG2A9P3dPHXaTYrwu/giphy.gif";
+    } else if (bonus >= 100) {
+        gif = "https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif";
+    } else if (bonus = 0) {
         gif = "https://media.giphy.com/media/ckGndVa23sCk9pae4l/giphy.gif";
+    } else {
+        gif = "https://media.giphy.com/media/3oz8xAFtqoOUUrsh7W/giphy.gif";
+    }
+
+    // Display bonus
+    const bonusElement = document.getElementById("bonus");
+    if (bonus > 0) {
+        bonusElement.innerHTML = `<strong>Your bonus is \u20AC${bonus.toFixed(
+            2
+        )}</strong>`;
+    } else {
+        bonusElement.textContent =
+            "Sorry, you don't qualify for a bonus this month.";
     }
 
     // Display the GIF
@@ -48,6 +72,18 @@ function calculateBonus() {
     setTimeout(() => {
         gifElement.style.display = "none";
     }, 3000);
+
+    // Save results to local storage
+    const date = new Date().toLocaleDateString();
+    const result = { date, closedChats, csat, position, bonus };
+    results.unshift(result);
+    localStorage.setItem("results", JSON.stringify(results));
+
+    // Add result to table
+    const row = resultsTable.insertRow(0);
+    row.innerHTML = `<td>${date}</td><td>${closedChats}</td><td>${csat}</td><td>${position}</td><td>${bonus.toFixed(
+        2
+    )} \u20AC</td>`;
 }
 
 const form = document.querySelector("form");
@@ -55,3 +91,4 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     calculateBonus();
 });
+
